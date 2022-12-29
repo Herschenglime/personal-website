2113180f2add9466609384db90a2b20a9c8165cc
Herschenglime <herschenglime@gmail.com>
2022-12-29 03:26:58 -0500

    merge sort done, on the site

--- src/routes/code-playground/+page.svelte
+++ src/routes/code-playground/+page.svelte
@@ -1,22 +1,109 @@
 <script>
-    import Prism from 'svelte-prism'
+ import Prism from 'svelte-prism'
+ import Toc from 'svelte-toc'
+ import * as ms from './mergeSort'
+
+ let randomArray = ms.generateRandomArray(10)
 </script>
 
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-okaidia.min.css'>
 
-<h1>Code Playground</h1>
-<p>At the time of writing, I had a technical interview coming up, so I wanted an interactive place to work through and store various coding challenges. Individual attempts are below. Code formatting is done with svelte-prism, an implementation of prism-js for svelte.</p>
+<!-- <Toc/> -->
 
-<h2>Initial code test</h2>
-This one's pretty simple, I'm just getting the hang of inline code formatting with prism.
-<Prism language="js">
-  {`let test = "test"`}
-</Prism>
+<main>
+  <h1>Code Playground</h1>
+  <p>At the time of writing, I had a technical interview coming up, so I wanted an interactive place to work through and store various coding challenges. Individual attempts are below. Code formatting is done with svelte-prism, an implementation of prism-js for svelte.</p>
 
-<h2>Merge Sort</h2>
-I learned about merge sort in discrete math, and have a pretty good recollection of implementing it recursively. Below is my attempt at an implementation in JS from memory, although I might have to look up the names of js array methods.
+  <h2>Merge Sort</h2>
+  <p>I learned about merge sort in discrete math, and have a pretty good recollection of implementing it recursively. I did have to quickly check up on the algorithm and ran into some js syntax errors, but the code below results in a functional merge sort for numbers.</p>
+  <Prism language="js">
+    {`function orderedMergeBrute(arr1, arr2) {
+      let sortedArr = arr1.concat(arr2)
+      return sortedArr.sort()
+    }
 
-(it's going a bit rough right now, will return later)
+    //brute test
+    {
+      let arr1 = [1, 2, 4, 9]
+      let arr2 = [3, 5, 6, 8]
 
-<h2>Merging Two Ordered Arrays</h2>
-This one seems to mostly rely on keeping track of indexes and bounds checking - shouldn't be too bad.
+      let sorted = orderedMergeBrute(arr1, arr2)
+    }
+
+    function orderedMerge(arr1, arr2) {
+      let sortedArr = []; // initialize as empty so that the length checking works
+
+      let i, j  //iterators
+      i = j = 0
+
+      while (sortedArr.length < (arr1.length + arr2.length)) {
+
+        if (i >= arr1.length) {
+          sortedArr.push(arr2[j++])
+          continue
+        }
+
+        if (j >= arr2.length) {
+          sortedArr.push(arr1[i++])
+          continue
+        }
+
+        if (arr1[i] < arr2[j]) {
+          sortedArr.push(arr1[i++])
+        } else {
+          sortedArr.push(arr2[j++])
+        }
+
+      }
+
+      return sortedArr
+    }
+
+    {
+      let arr1 = [1, 3, 4, 5]
+      let arr2 = [2, 6, 9, 10]
+
+      orderedMerge(arr1, arr2)
+
+      let arr3 = [1, 2, 3, 4]
+      let arr4 = []
+
+      orderedMerge(arr3,arr4)
+    }
+
+    //merge sort time, woot!
+    function mergeSort(arr) {
+      let leftArr = []
+      let rightArr = []
+
+      if (arr.length > 1) {
+        let halfway = arr.length / 2
+
+        //slice arrays into atomic units
+        leftArr = mergeSort(arr.slice(0, halfway))
+        rightArr = mergeSort(arr.slice(halfway))
+
+      } else {
+        leftArr = arr // just set left to what was passed in
+      }
+
+      //reconstruct sorted array
+      return orderedMerge(leftArr, rightArr)
+    }
+
+    //test block
+    {
+      let unsortedArr = [4, 23, 7, 39, 19, 0, 9, 14]
+      let sortedArr = mergeSort(unsortedArr)
+    }
+
+    //random array generator to test sorting, taken from https://stackoverflow.com/a/43044960
+    function generateRandomArray(arrLength) {
+      return Array.from({length: arrLength}, () => Math.floor(Math.random() * arrLength));
+    }
+    `}
+  </Prism>
+  <h3>Examples</h3>
+    <p>Random Array: {randomArray}</p>
+
+</main>
--- src/routes/code-playground/mergeSort.js
+++ src/routes/code-playground/mergeSort.js
@@ -1,128 +1,4 @@
-// @ts-nocheck
-// ^ above stops thing from getting angry
-
-let testArr1 = [1, 3, 54, 9, 2, 17, 22, 700]
-
-/*
- * original, unsorted array
- *
- * split it up into 2 halves, then split those halves in half until you're left with a bunch of individual split arrays
- *
- * Then, reconstruct them in order, until the final array is done
- */
-
-//will make tail recursive if possible
-function mergeSort(arr) {
-  if (arr.length > 1) {
-    let halfway = arr.length / 2
-
-    //slice arrays into atomic units
-    let leftArr = mergeSort(arr.slice(0, halfway))
-    let rightArr = mergeSort(arr.slice(halfway))
-
-  }
-
-  //reconstruct sorted array
-  let sortedIndex = 0
-
-  let leftIterator, rightIterator = 0
-
-  while (sortedIndex < arr.length) {
-    if (leftArr[leftIterator] < rightArr[rightIterator]) sortedArr.push(leftArr[leftIterator++])
-    else
-      sortedArr.push(rightArr[rightIterator++])
-
-    sortedIndex++
-  }
-}
-
-// repl stuff
-// // @ts-nocheck
-// ^ above stops thing from getting angry
-
-
-let undefinedTestArr = [0 , 1 , 3]
-
-
-/*
- * original, unsorted array
- *
- * split it up into 2 halves, then split those halves in half until you're left with a bunch of individual split arrays
- *
- * Then, reconstruct them in order, until the final array is done
- */
-
-function arrSplit(arr) {
-    if (arr.length < 2) {
-        return {left: arr, right:undefined} //returns original array in left if it cannot be split
-    } else {
-        let halfway = arr.length / 2
-        let leftArr = arr.slice(0, halfway)
-        let rightArr = arr.slice(halfway)
-
-        return {left: leftArr, right: rightArr}
-    }
-}
-
-//block scoped test cases
-{
-let splitTest = [1, 2, 3, 4]
-arrSplit(splitTest)
-}
-
-{
-let singleSplit = [1]
-arrSplit(singleSplit)
-}
-
-{
-let unevenSplitTest = [1, 2, 3, 4, 5]
-arrSplit(unevenSplitTest)
-}
-
-
-//will make tail recursive if possible
-function mergeSort(arr) {
-  if (arr.length > 1) {
-    let halfway = arr.length / 2
-
-    //slice arrays into atomic units
-    let leftArr = mergeSort(arr.slice(0, halfway))
-    let rightArr = mergeSort(arr.slice(halfway))
-
-  }
-
-  //reconstruct sorted array
-  let sortedArr
-  let leftIterator, rightIterator = 0
-
-  while (sortedArr.length < arr.length) {
-    if (leftIterator >= leftArr.length) {
-        sortedArr.push(rightIterator++)
-        continue
-    }
-
-    if (rightIterator >= rightArr.length) {
-        sortedArr.push(leftIterator)
-        continue
-    }
-
-    if (leftArr[leftIterator] < rightArr[rightIterator])
-        sortedArr.push(leftArr[leftIterator++])
-    else
-        sortedArr.push(rightArr[rightIterator++])
-  }
-
-  return sortedArr;
-}
-
-let testArr1 = [1, 3, 54, 9, 2, 17, 22, 700]
-
-
-console.log(mergeSort(testArr1))
-
-
-//merge 2 ordered arrays
+//"cheats" on sorting by using built in methods
 function orderedMergeBrute(arr1, arr2) {
     let sortedArr = arr1.concat(arr2)
     return sortedArr.sort()
@@ -136,11 +12,11 @@ function orderedMergeBrute(arr1, arr2) {
     let sorted = orderedMergeBrute(arr1, arr2)
 }
 
-function orderedMerge(arr1, arr2) {
+export function orderedMerge(arr1, arr2) {
     let sortedArr = []; // initialize as empty so that the length checking works
 
-    let i, j
-    i = j = 0 //iterators
+    let i, j  //iterators
+    i = j = 0
 
     while (sortedArr.length < (arr1.length + arr2.length)) {
 
@@ -170,4 +46,40 @@ function orderedMerge(arr1, arr2) {
     let arr2 = [2, 6, 9, 10]
 
     orderedMerge(arr1, arr2)
+
+    let arr3 = [1, 2, 3, 4]
+    let arr4 = []
+
+    orderedMerge(arr3,arr4)
+}
+
+//merge sort time, woot!
+export function mergeSort(arr) {
+    let leftArr = []
+    let rightArr = []
+
+    if (arr.length > 1) {
+        let halfway = arr.length / 2
+
+        //slice arrays into atomic units
+        leftArr = mergeSort(arr.slice(0, halfway))
+        rightArr = mergeSort(arr.slice(halfway))
+
+    } else {
+        leftArr = arr // just set left to what was passed in
+    }
+
+  //reconstruct sorted array
+  return orderedMerge(leftArr, rightArr)
+}
+
+//test block
+{
+    let unsortedArr = [4, 23, 7, 39, 19, 0, 9, 14]
+    let sortedArr = mergeSort(unsortedArr)
+}
+
+//random array generator to test sorting, taken from https://stackoverflow.com/a/43044960
+export function generateRandomArray(length) {
+  Array.from({length: length}, () => Math.floor(Math.random() * length));
 }
