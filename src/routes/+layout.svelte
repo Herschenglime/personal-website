c6c0f9e2742528231bc04b14fbc9a37b2c5e04a8
Herschenglime <herschenglime@gmail.com>
2022-12-29 00:24:18 -0500

    post-oopsie restoral, working links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 
  import "../app.scss";
- import Navbar from "./Navbar.svelte";
+ import Navbar from "$lib/components/Navbar.svelte";
+
+ //theme switching
+ import { theme } from '$lib/stores'
+
 </script>
 
+<svelte:head>
+  <meta name="color-scheme" content={$theme == 'system' ? 'light dark' :
+  $theme}/> <link rel="stylesheet" href={`/theme/${$theme}.css`} />
+</svelte:head>
 
 <Navbar/>
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Home</h1>
+<h2>What?</h2>
+Cool website to host details about me, portfolio, code playground
+<h2>Website details</h2>
+TS, svelte, sveltekit
+
+<h2>Links!!!</h2>
+<nav>
+  <a href="/code-playground">Code Playground</a>
+</nav>
